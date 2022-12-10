6a8a7a470cecf2ef171754c96ed905c0d3b01c9d
Kalim <kalim.ahmetchin2003@gmail.com>
2022-12-10 15:20:00 +0300

    add clients and server

--- semestr-2/SpaceInvadersClient/src/main/java/org/example/AwesomeClient.java
+++ semestr-2/SpaceInvadersClient/src/main/java/org/example/AwesomeClient.java
@@ -1,6 +1,8 @@
 package org.example;
 
-import java.io.*;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.Socket;
 
 public class AwesomeClient {
---
+++ semestr-2/SpaceInvadersServer/src/main/java/org/example/AwesomeServer.java
@@ -0,0 +1,96 @@
+package org.example;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ThreadPoolExecutor;
+
+public class AwesomeServer implements Runnable {
+    private Integer port;
+    private ServerSocket serverSocket;
+    private Socket clientSocket;
+
+    private ThreadPoolExecutor serverPool;
+
+    private InputStream inputStream;
+    private OutputStream outputStream;
+
+    private ObjectMapper mapper = new ObjectMapper();
+
+    private List<Socket> sockets = new ArrayList<>();
+    private AwesomeServer() {}
+
+    public static AwesomeServer create(Integer port, ThreadPoolExecutor serverPool) throws IOException {
+        AwesomeServer server = new AwesomeServer();
+        server.port = port;
+        server.serverSocket = new ServerSocket(port);
+        server.serverPool = serverPool;
+        return server;
+    }
+
+    private byte[] extendArray(byte[] oldArray) {
+        int oldSize = oldArray.length;
+        byte[] newArray = new byte[oldSize * 2];
+        System.arraycopy(oldArray, 0, newArray, 0, oldSize);
+        return newArray;
+    }
+
+    private byte[] readInput(InputStream stream) throws IOException {
+        int b;
+        byte[] buffer = new byte[10];
+        int counter = 0;
+        while ((b = stream.read()) > -1) {
+            buffer[counter++] = (byte) b;
+            if (counter >= buffer.length) {
+                buffer = extendArray(buffer);
+            }
+            if (counter > 2 && SuperPacket.compareEndOfPacket(buffer, counter - 1)) {
+                break;
+            }
+        }
+        byte[] data = new byte[counter];
+        System.arraycopy(buffer, 0, data, 0, counter);
+        return data;
+    }
+
+
+    @Override
+    public void run() {
+        try {
+            clientSocket = serverSocket.accept();
+            outputStream = clientSocket.getOutputStream();
+            inputStream = clientSocket.getInputStream();
+
+            System.out.println("connected");
+
+            sockets.add(clientSocket);
+
+            System.out.println(Thread.getAllStackTraces());
+
+            SuperPacket handshake = SuperPacket.create(1);
+            outputStream.write(handshake.toByteArray());
+            outputStream.flush();
+
+            while (true) {
+
+//                if (bye.getType() == 4) {
+//                    System.out.println("bye");
+//                    synchronized (serverPool) {
+//                        serverPool.notifyAll();
+//                    }
+//
+//                    break;
+//                }
+
+            }
+        } catch (IOException e) {
+            throw new IllegalArgumentException(e);
+        }
+    }
+}
